2027982290249a4707c96092dd44ccc28c260b0d
Andrei Lavrenov <andrei@lavrenov.io>
2025-05-10 12:22:19 +0200

    Configure Docker
    - next.config.ts to output nextjs for a docker-based runtime.
    - Dockerfile to build the image
    - .dockerignore to make the image as lean as possible
    - docker-compose.yml to run the image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "standalone", // #1
 };
 
 export default nextConfig;
+
+// #1
+
+// Next.js can automatically create a standalone folder that copies only the necessary files for a production deployment including select files in node_modules.
+// To leverage this automatic copying you can enable it in your next.config.js:
+// This will create a folder at .next/standalone which can then be deployed on its own without installing node_modules.
+
+// Additionally, a minimal server.js file is also output which can be used instead of next start.
+// This minimal server does not copy the public or .next/static folders by default as these should ideally be handled by a CDN instead,
+// although these folders can be copied to the standalone/public and standalone/.next/static folders manually,
+// after which server.js file will serve these automatically.
