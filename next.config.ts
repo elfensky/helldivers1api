1128ad6c1de5d3961a357dd091fb8a874160aecc
Andrei Lavrenov <andrei@lavrenov.io>
2025-05-10 12:34:36 +0200

    Configure Prettier & Volta + Format
    volta
    - pin node@22.15.0
    - pin npm@11.3.0
    
    prettier
    - install prettier and prettier-plugin-tailwindcss
    - configure .prettierrc
    - configure .prettierignore
    
    format all files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    output: "standalone", // #1
+    output: 'standalone', // #1
 };
 
 export default nextConfig;
