ed8a068502911065ee4eb4b4671deb778a23d536
Andrei Lavrenov <andrei@lavrenov.io>
2025-05-20 11:50:56 +0200

    migrate to swagger-ui-dist
    swagger-ui-react doesnt properly support react19 yet

--- src/app/docs/page.jsx
+++ src/app/docs/page.jsx
@@ -1,11 +1,31 @@
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+'use client';
+import { useEffect, useRef } from 'react';
+// import SwaggerUI from 'swagger-ui-react'; //incompatible with React19 (yet)
+// import 'swagger-ui-react/swagger-ui.css';
+import { SwaggerUIBundle } from 'swagger-ui-dist';
+import 'swagger-ui-dist/swagger-ui.css';
+import './swaggerDark.css'; // see: https://github.com/Amoenus/SwaggerDark/
 
-export default function HomePage() {
-    const specFile = '/api/openapi';
-    return (
-        <div className="bg-amber-100">
-            <SwaggerUI url={specFile} />
-        </div>
-    );
+// export default function Docs() {
+//     const specFile = '/api/openapi';
+//     return (
+//         <div className="bg-amber-100">
+//             <SwaggerUI url={specFile} />
+//         </div>
+//     );
+// }
+
+const spec = await fetch('/api/openapi').then((res) => res.json());
+export default function Docs() {
+    const swaggerRef = useRef(null);
+
+    useEffect(() => {
+        if (!swaggerRef.current || !spec) return;
+        SwaggerUIBundle({
+            domNode: swaggerRef.current,
+            spec,
+        });
+    }, [spec]);
+
+    return <div className="swagger-ui-wrapper" ref={swaggerRef} />;
 }
