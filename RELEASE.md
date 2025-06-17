e1aaa933bde631f6723d6b8f52d06360d4521af3
Andrei Lavrenov <andrei@lavrenov.io>
2025-06-17 08:41:21 +0200

    Fix Galaxy - Hidden Campaigns & in_progress styling
    - Hidden campaigns now correctly display as 0 progress
    - "in_progress" (contested region) doesn't pulse red. Only "active" (Defend & Attack Events) should pulse red.

--- RELEASE.md
+++ RELEASE.md
@@ -1,4 +1,6 @@
-## 0.7.1 (2025-06-16)
+## 0.7.1 (2025-06-17)
 
-Update Umami tracking code(s)
-Update Tooltip to always show inside body
+- Update Umami tracking code(s)
+- Update Tooltip to always show inside body
+- Hidden campaigns now correctly display as 0 progress
+- "in_progress" (contested region) doesn't pulse red. Only "active" (Defend & Attack Events) should pulse red.
--- src/components/h1/Galaxy/Galaxy.jsx
+++ src/components/h1/Galaxy/Galaxy.jsx
@@ -42,6 +42,7 @@ export default function Galaxy({ data, rebroadcast }) {
 
 function processCampaigns(data) {
     data?.campaigns?.forEach((campaign, faction) => {
+        // console.log(campaign);
         const sector_count = 10; //10 sectors, 11th (homeworld) is determined by the attack event, not campaign progress.
         const points_max = campaign?.points_max;
         const points = campaign?.points;
@@ -51,54 +52,62 @@ function processCampaigns(data) {
         const sectors_in_progress = sectors_earned + 1;
         const sectors_remaining = sector_count - sectors_earned - 1;
 
-        for (const region in map[faction]) {
-            const total_points_for_sector = region * points_per_sector;
+        if (campaign.status === 'active') {
+            for (const region in map[faction]) {
+                const total_points_for_sector = region * points_per_sector;
 
-            if (region === '11') {
-                //this will later be overwritten by processAttackEvents
-                map[faction][region].status = 'lost';
-                map[faction][region].percent = 0;
-                // if (campaign.status === 'defeated') {
-                //     map[faction][region].status = 'captured';
-                //     map[faction][region].percent = 100;
-                // } else {
-                //     map[faction][region].status = 'lost';
-                //     map[faction][region].percent = 0;
-                //
-                // }
-            } else if (region === sectors_in_progress.toString()) {
-                //if in progress, calculate module difference
-                const remaining_points =
-                    points - (total_points_for_sector - points_per_sector);
+                if (region === '11') {
+                    //this will later be overwritten by processAttackEvents
+                    map[faction][region].status = 'lost';
+                    map[faction][region].percent = 0;
+                    // if (campaign.status === 'defeated') {
+                    //     map[faction][region].status = 'captured';
+                    //     map[faction][region].percent = 100;
+                    // } else {
+                    //     map[faction][region].status = 'lost';
+                    //     map[faction][region].percent = 0;
+                    //
+                    // }
+                } else if (region === sectors_in_progress.toString()) {
+                    //if in progress, calculate module difference
+                    const remaining_points =
+                        points - (total_points_for_sector - points_per_sector);
 
-                map[faction][region].status = 'in_progress';
-                map[faction][region].points = points;
-                map[faction][region].points_max = total_points_for_sector;
-                map[faction][region].points_sector = remaining_points;
-                map[faction][region].points_sector_max = points_per_sector;
-                //percentage
-                const percent = (remaining_points / points_per_sector) * 100;
-                map[faction][region].percent = percent;
-                //
-            } else if (region <= sectors_earned) {
-                //if captured, simply use the totals per sector.
-                map[faction][region].status = 'captured';
-                map[faction][region].points = total_points_for_sector;
-                map[faction][region].points_max = total_points_for_sector;
-                map[faction][region].points_sector = points_per_sector;
-                map[faction][region].points_sector_max = points_per_sector;
-                //percentage
-                const percent = (total_points_for_sector / total_points_for_sector) * 100;
-                map[faction][region].percent = percent;
-                //
-            } else {
-                //if it's here, it hasn't been reached yet, and the points are 0;
+                    map[faction][region].status = 'in_progress';
+                    map[faction][region].points = points;
+                    map[faction][region].points_max = total_points_for_sector;
+                    map[faction][region].points_sector = remaining_points;
+                    map[faction][region].points_sector_max = points_per_sector;
+                    //percentage
+                    const percent = (remaining_points / points_per_sector) * 100;
+                    map[faction][region].percent = percent;
+                    //
+                } else if (region <= sectors_earned) {
+                    //if captured, simply use the totals per sector.
+                    map[faction][region].status = 'captured';
+                    map[faction][region].points = total_points_for_sector;
+                    map[faction][region].points_max = total_points_for_sector;
+                    map[faction][region].points_sector = points_per_sector;
+                    map[faction][region].points_sector_max = points_per_sector;
+                    //percentage
+                    const percent =
+                        (total_points_for_sector / total_points_for_sector) * 100;
+                    map[faction][region].percent = percent;
+                    //
+                } else {
+                    //if it's here, it hasn't been reached yet, and the points are 0;
+                    map[faction][region].status = 'lost';
+                    map[faction][region].points = points;
+                    map[faction][region].points_max = total_points_for_sector;
+                    map[faction][region].points_sector = 0;
+                    map[faction][region].points_sector_max = points_per_sector;
+                    //percentage
+                    map[faction][region].percent = 0;
+                }
+            }
+        } else {
+            for (const region in map[faction]) {
                 map[faction][region].status = 'lost';
-                map[faction][region].points = points;
-                map[faction][region].points_max = total_points_for_sector;
-                map[faction][region].points_sector = 0;
-                map[faction][region].points_sector_max = points_per_sector;
-                //percentage
                 map[faction][region].percent = 0;
             }
         }
@@ -106,32 +115,36 @@ function processCampaigns(data) {
 }
 
 function processDefendEvents(data) {
-    data?.defend_events?.forEach((event) => {
-        if (event?.status === 'active') {
-            map[event?.enemy][event?.region].event = 'active';
-        } else {
-            map[event?.enemy][event?.region].event = 'idle';
-        }
-    });
+    if (data?.defend_events?.length > 0) {
+        data?.defend_events?.forEach((event) => {
+            if (event?.status === 'active') {
+                map[event?.enemy][event?.region].event = 'active';
+            } else {
+                map[event?.enemy][event?.region].event = 'idle';
+            }
+        });
+    }
 }
 
 function processAttackEvents(data) {
-    data?.attack_events?.forEach((event) => {
-        if (event?.status === 'active') {
-            map[event?.enemy][11].percent = (event?.points / event?.points_max) * 100;
-            map[event?.enemy][11].points = event?.points;
-            map[event?.enemy][11].points_max = event?.points_max;
-            // map[event?.enemy][11].points_sector = event?.points;
-            // map[event?.enemy][11].points_sector_max = event?.points_max;
-            map[event?.enemy][11].status = 'in_progress';
-            map[event?.enemy][11].event = event;
-        }
-        if (event?.status === 'success') {
-            map[event?.enemy][11].percent = (event?.points / event?.points_max) * 100;
-            map[event?.enemy][11].points = event?.points;
-            map[event?.enemy][11].points_max = event?.points_max;
-            map[event?.enemy][11].status = 'captured';
-            map[event?.enemy][11].event = event;
-        }
-    });
+    if (data?.attack_events?.length > 0) {
+        data?.attack_events?.forEach((event) => {
+            if (event?.status === 'active') {
+                map[event?.enemy][11].percent = (event?.points / event?.points_max) * 100;
+                map[event?.enemy][11].points = event?.points;
+                map[event?.enemy][11].points_max = event?.points_max;
+                // map[event?.enemy][11].points_sector = event?.points;
+                // map[event?.enemy][11].points_sector_max = event?.points_max;
+                map[event?.enemy][11].status = 'in_progress';
+                map[event?.enemy][11].event = event;
+            }
+            if (event?.status === 'success') {
+                map[event?.enemy][11].percent = (event?.points / event?.points_max) * 100;
+                map[event?.enemy][11].points = event?.points;
+                map[event?.enemy][11].points_max = event?.points_max;
+                map[event?.enemy][11].status = 'captured';
+                map[event?.enemy][11].event = event;
+            }
+        });
+    }
 }
